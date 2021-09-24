4a67e91ad8e54e2a90f97dbecc2682935109ff52
kavindaperera <perera.kavinda52@gmail.com>
2021-09-24 16:01:12 +0530

    Refactor Code

--- src/senatebus/Main.java
+++ src/senatebus/Main.java
@@ -5,9 +5,9 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
-    private static int MAX_RIDERS = 50;
+    private static final int MAX_RIDERS = 50;
     private static final float arrivalTimeBusses = 20 * 60 * 10; //10->1000
     private static final float arrivalTimeRiders = 30 * 10; //10->1000
 
@@ -105,10 +105,10 @@ public class Main {
         }
     }
 
-    private static long getExpoDistributedTimeFromMeanTime(float arrivalMeanTime) {
-        float lambda = 1 / arrivalMeanTime;
-        float p = random.nextFloat();
-        return Math.round(Math.log(1 - p) / (-lambda));
+    private static long getExpoDistributedTimeFromMeanTime(float arrivalMeanTime) { // inverse transform sampling
+        float lambda = 1 / arrivalMeanTime; // rate
+        float p = random.nextFloat(); // generate uniform random number in [0,1]
+        return Math.round(Math.log(1 - p) / (-lambda)); // return exponentially distributed value
     }
 
 }
